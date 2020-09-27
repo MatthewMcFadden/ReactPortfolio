235b8f012202e20d5d11f06da27454af9bf0a30a
Matt M <matt.mcfadden14@gmail.com>
2020-09-27 11:10:40 -0600

    Updates

---
+++ precache-manifest.df1f86d63b23ce86d04900195d3f08b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6fb919dddfdc02bf75969a9a3fbe39",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "188ae2495be03b3597c9",
+    "url": "/ReactPortfolio/static/css/main.bf83a67c.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/ReactPortfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPortfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188ae2495be03b3597c9",
+    "url": "/ReactPortfolio/static/js/main.a9d78bf7.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  }
+]);
