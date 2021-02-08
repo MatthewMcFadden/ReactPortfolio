c1c6cb2e3f2d7aff6f0da0412d0e7e9df997b9fe
Matt M <matt.mcfadden14@gmail.com>
2021-02-08 12:37:33 -0700

    Updates

--- precache-manifest.4a9aae29ff1fadb61a7a513be6206d27.js
+++ precache-manifest.4a9aae29ff1fadb61a7a513be6206d27.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d1f1ad6b9ca8720ca437b8ca6800dad",
+    "revision": "18b14add383230b30789374999d5712a",
     "url": "/ReactPortfolio/index.html"
   },
   {
-    "revision": "e6c7cb3d3b2087cb1306",
+    "revision": "ee3533d54d0732adbf4c",
     "url": "/ReactPortfolio/static/css/main.bf83a67c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ReactPortfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e6c7cb3d3b2087cb1306",
-    "url": "/ReactPortfolio/static/js/main.af54dc15.chunk.js"
+    "revision": "ee3533d54d0732adbf4c",
+    "url": "/ReactPortfolio/static/js/main.38bb87d4.chunk.js"
   },
   {
     "revision": "97a1c7d0b592075495b7",
